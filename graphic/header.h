41ea8fb10232b88e17ee1e80404324f073cbdb85
Mohamed Takrayout <mohtakra@e2r7p5.1337.ma>
2023-02-24 18:17:14 +0100

    the learn of minilibx

---
+++ graphic/header.h
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+// #include <unistd.h>
+#include <mlx.h>
+typedef struct s_data {
+    void    *img;
+    char    *addr;
+    int     bits_per_pixel;
+    int     line_length;
+    int     endian;
+}   t_data;
+
+typedef struct s_vars {
+    void    *mlx;
+    void    *win;
+}       t_vars;
+
+/*this used only while you are working with events*/
+enum {
+	ON_KEYDOWN = 2,
+	ON_KEYUP = 3,
+	ON_MOUSEDOWN = 4,
+	ON_MOUSEUP = 5,
+	ON_MOUSEMOVE = 6,
+	ON_EXPOSE = 12,
+	ON_DESTROY = 17
+};
--- graphic/win.c
+++ graphic/win.c
@@ -1,15 +1,62 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <mlx.h>
+#include "header.h"
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+    // printf("data->bits_per_pixel = %d\n",data->bits_per_pixel);
+    // printf("data->line_length = %d\n",data->line_length);
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+int	close(int keycode, t_vars *vars)
+{
+    // (void)keycode;
+    printf("key code = %d",keycode);
+	mlx_destroy_window(vars->mlx, vars->win);
+	return (0);
+}
+
+int press_key(int keycode)
+{
+    printf("hello world key code = %d \n", keycode);
+    return (0);
+}
+
+int press_mouse(int keycode)
+{
+    printf("the key is : %d\n", keycode);
+    return (0);
+}
+
 int main(void)
 {
-    void    *mlx;
-    void    *mlx_win;
+    t_vars  vars;
+    t_data  img;
+    int     i = 0;
 
-    mlx = mlx_init();
-    mlx_win = mlx_new_window(mlx, 2000, 1000, "hello alvares");
-    mlx_loop(mlx);
+    vars.mlx = mlx_init();
+    vars.win = mlx_new_window(vars.mlx, 800, 600, "hello alvares");
+    img.img = mlx_new_image(vars.mlx, 400, 400);
+    img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+
+    while(i < 20)
+    {
+        my_mlx_pixel_put(&img, 50 + i, 50, 0x00FF0000);
+        // sleep(1);
+        i++;
+    }
+    mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
+    /*x_mask argument of mlx_hook is useless, keep it at 0*/
+    // mlx_hook(vars.win, ON_KEYDOWN, 0, close, &vars);
+
+
+    /*even it looks a new function but it only calls the function mlx_hook() 
+        with the appropriate X11 event types*/
+    mlx_key_hook(vars.win, press_key, &vars);
+
+    mlx_mouse_hook(vars.win, press_mouse, &vars);
+
+    mlx_loop(vars.mlx);
     return 0;
 }
