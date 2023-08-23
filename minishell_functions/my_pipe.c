b5bf180d4f279290928cac8b0a74009f7bcd3733
TakrayoutMohamed <takrayoutm@gmail.com>
2023-08-23 19:03:20 +0100

    this is teest to learn how to pass data from a pipe to another at the same process

--- minishell_functions/my_pipe.c
+++ minishell_functions/my_pipe.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <errno.h>
+#include <wait.h>
 #include <string.h>
 
 int status;
@@ -9,13 +10,13 @@ int status;
 /*this function read from a fd_in's content and write it to fd_out*/
 void	read_from_fd(int fd_in, int fd_out)
 {
-	char	buff;
-	size_t	readedbuf;
 
 	while (1)
 	{
+		char	buff;
+		size_t	readedbuf;
 		readedbuf = read(fd_in, &buff, 1);
-		if (readedbuf == 0 || readedbuf == -1)
+		if (readedbuf == -1)
 		{
 			exit(errno);
 		}
@@ -93,6 +94,80 @@ void	ft_pipe_begain(char *cmd1[], char *cmd2[])
 	}
 }
 
+void ft_pipe_one_process()
+{
+	int p1[2];
+	int p2[2];
+
+	if (pipe(p1) == -1)
+	{
+		print_error(errno);
+		exit(errno);
+	}
+	if (pipe(p2) == -1)
+	{
+		print_error(errno);
+		exit(errno);
+	}
+
+	dup(p1[1]);
+	dup(p2[1]);
+	write(p1[1], "hello this is going to pipe 1\ndd", 33);
+	dup2(p1[0], 0);
+	// char c   = '\0';
+	// write(p1[1],&c,1);
+	close(p1[1]);
+	close(p1[0]);
+	print_error(errno);
+	char	buff[2];
+	size_t	readedbuf;
+	while (1)
+	{
+		// write(2, "HHH1\n",6);
+		readedbuf = read(0, &buff, 2);
+		// write(2, "HHH2\n",6);
+		// fprintf(stderr,"\nreadedbuf = |%ld|\n",readedbuf);
+		if (readedbuf == -1)
+		{
+			print_error(errno);
+			exit(errno);
+		}
+		if (readedbuf == 0)
+		{
+			write(2, "EOF\n",5);
+			break;
+		}
+		if (write(p2[1], &buff, 2) != 2)
+			break;
+		if ((unsigned char)buff[readedbuf - 1] == '\0')
+			break;
+	}
+	// close(p1[0]);
+	write(p2[1], "hello this is going to pipe 2\nhanta", 36);
+	close(p2[1]);
+	while (1)
+	{
+		// write(2, "HHH1\n",6);
+		readedbuf = read(p2[0], &buff, 2);
+		// write(2, "HHH2\n",6);
+		// fprintf(stderr,"\nreadedbuf = |%ld|\n",readedbuf);
+		if (readedbuf == -1)
+		{
+			print_error(errno);
+			exit(errno);
+		}
+		if (readedbuf == 0)
+		{
+			write(2, "EOF\n",5);
+			break;
+		}
+		if (write(1, &buff, 2) != 2)
+			break;
+		if ((unsigned char)buff[readedbuf -1] == '\0')
+			break;
+	}
+}
+
 int main(void)
 {
 	int	pipe1[2];
@@ -105,9 +180,10 @@ int main(void)
     char *cmd2[] = {"/usr/bin/grep", "read", NULL};
     char *cmd3[] = {"/usr/bin/sort", "-r" , NULL};
     char *cmd4[] = {"/usr/bin/sort", NULL, NULL};
-    ft_pipe_begain(cmd1, cmd2);
+    // ft_pipe_begain(cmd1, cmd2);
+	ft_pipe_one_process();
     // ft_pipe_begain(cmd3, cmd4);
-	printf("\nhello\n\n");
+	// printf("\nhello\n\n");
 	// read_from_fd(0, 1);
     return (0);
 }
