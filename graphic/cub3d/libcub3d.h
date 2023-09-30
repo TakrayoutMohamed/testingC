2ac7dd0c5024a2caa50cd534aba2e5b9da13c326
TakrayoutMohamed <takrayoutm@gmail.com>
2023-09-30 19:09:14 +0100

    asfd

--- graphic/cub3d/libcub3d.h
+++ graphic/cub3d/libcub3d.h
@@ -5,7 +5,15 @@
 # include <math.h>
 # include <string.h>
 # include <mlx.h>
+# define WIN_WIDTH 800
+# define WIN_HEIGHT 600
+# define PLAYER_X WIN_WIDTH/2
+# define PLAYER_Y WIN_HEIGHT/2
 
-
+typedef struct data {
+	void	*mlx;
+	void	*win;
+	void	*img;
+} t_data;
 
 #endif
--- graphic/cub3d/main.c
+++ graphic/cub3d/main.c
@@ -1,6 +1,43 @@
 # include "./libcub3d.h"
 
+void	trace_dot(t_data *vars, int x,int y)
+{
+	mlx_pixel_put(vars->mlx, vars->win, x, y, 0xFFFFFF);
+}
+
+void	trace_ray(t_data *vars)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (i < WIN_WIDTH - PLAYER_X)
+	{
+		trace_dot(vars, PLAYER_X + i, PLAYER_Y + j);
+		i++;
+		if (i == WIN_WIDTH - PLAYER_X)
+		{
+			i = 0;
+			j++;
+		}
+		if (j == WIN_HEIGHT - PLAYER_Y)
+		{
+			printf("done!\n");
+			break ;
+		}
+	}
+}
+
 int	main(void)
 {
+	t_data	vars;
+
+	vars.mlx = mlx_init();
+	if (vars.mlx == NULL)
+		return (EXIT_FAILURE);
+	vars.win = mlx_new_window(vars.mlx, WIN_WIDTH, WIN_HEIGHT, "alvares's cub3d");
+	trace_ray(&vars);
+	mlx_loop(vars.mlx);
 	return (0);
 }
