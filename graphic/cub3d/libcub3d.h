c301c1ecc76e3ab014f7eba65830d5b312ea7ae9
TakrayoutMohamed <takrayoutm@gmail.com>
2023-10-01 19:37:22 +0100

    after printing the view

--- graphic/cub3d/libcub3d.h
+++ graphic/cub3d/libcub3d.h
@@ -3,14 +3,23 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <math.h>
+# include <unistd.h>
 # include <string.h>
 # include <mlx.h>
+# define PI 3.14
+#define RIGHT_ARROW  124
+#define LEFT_ARROW  123
+#define TOP_ARROW  126
+#define BOTTOM_ARROW  125
 # define WIN_WIDTH 800
 # define WIN_HEIGHT 600
 # define PLAYER_X WIN_WIDTH/2
 # define PLAYER_Y WIN_HEIGHT/2
 
 typedef struct data {
+	int		x_player;
+	int		y_player;
+	float	angle;
 	void	*mlx;
 	void	*win;
 	void	*img;
--- graphic/cub3d/main.c
+++ graphic/cub3d/main.c
@@ -12,32 +12,76 @@ void	trace_ray(t_data *vars)
 
 	i = 0;
 	j = 0;
-	while (i < WIN_WIDTH - PLAYER_X)
+	while (i < WIN_WIDTH - 100)
 	{
-		trace_dot(vars, PLAYER_X + i, PLAYER_Y + j);
-		i++;
-		if (i == WIN_WIDTH - PLAYER_X)
+		vars->y_player = sqrt(pow((vars->x_player + i) / cos(vars->angle), 2) - pow(vars->x_player + i, 2));
+		if (vars->y_player > WIN_HEIGHT - 20)
 		{
-			i = 0;
-			j++;
-		}
-		if (j == WIN_HEIGHT - PLAYER_Y)
-		{
-			printf("done!\n");
 			break ;
 		}
+		trace_dot(vars, vars->x_player + i, vars->y_player);
+		i++;
+	}
+	vars->y_player = 0;
+	printf("done!\n");
+}
+
+void	trace_view(t_data *vars)
+{
+	int	i;
+
+	i = 0;
+	vars->angle = 0;
+	while (i++ < 1100)
+	{
+		trace_ray(vars);
+		vars->angle = vars->angle + 0.01;
 	}
 }
+
+int	handle_keypress(int key, t_data *vars)
+{
+	(void) vars;
+	if (key == 53)
+	{
+		printf("ESC pressed \nexit\n");
+		exit(0);
+	}
+	else if (key == RIGHT_ARROW || key == LEFT_ARROW)
+	{
+		if (key == RIGHT_ARROW)
+		{
+			vars->angle = vars->angle + 0.01;
+			printf("the right arrow pressed \n");
+		}
+		else if (key == LEFT_ARROW)
+		{
+			vars->angle = vars->angle - 0.01;
+			printf("the left arrow pressed \n");
+		}
+		printf(" angle = %f\n", vars->angle);
+		trace_ray(vars);
+	}
+	return (0);
+}
+
 
 int	main(void)
 {
 	t_data	vars;
 
 	vars.mlx = mlx_init();
+	// the player is in the top left corner (0,0)
+	vars.x_player = 0;
+	vars.y_player = 0;
+	vars.angle = 3.14;
 	if (vars.mlx == NULL)
 		return (EXIT_FAILURE);
 	vars.win = mlx_new_window(vars.mlx, WIN_WIDTH, WIN_HEIGHT, "alvares's cub3d");
-	trace_ray(&vars);
+	// trace_ray(&vars);
+	trace_view(&vars);
+	// mlx_loop_hook(vars.mlx, render, &vars);
+	mlx_key_hook(vars.win, &handle_keypress, &vars);
 	mlx_loop(vars.mlx);
 	return (0);
 }
